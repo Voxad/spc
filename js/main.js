448d51d8544e83a2cc6ac427a936810e2295fe91
Voxad <apj@SOMONE-APJ.localdomain>
2017-11-07 10:45:20 +0100

    dev v1.0.1a

--- js/main.js
+++ js/main.js
@@ -1,3 +1,50 @@
+
+
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var lineWidth = 5;
+var cols = 50;
+var rows = 50;
+var tiles = [];
+
+function drawboard(){
+for (var i = 1; i <= rows; i++) {
+    for (var j = 1; j <= cols; j++) {
+        // Draw rect
+        ctx.beginPath();
+				ctx.lineWidth="1";
+				ctx.strokeStyle="red";
+        var x = i*10;
+        var y = j*10;
+				ctx.rect(x,y,10,10); 
+        ctx.stroke();
+				
+        
+        // push circle info as objects:
+        tiles.push({
+            id: '['+i+','+j+']',    // some ID
+            x: x,
+            y: y,
+        });
+      
+    }
+      
+}
+}
+
+
+canvas.onclick = function(e) {
+x = e.offsetX
+y = e.offsetY
+while (tile = tiles[i++]){
+if (x > tile.x && x < tile.x+10 && y > tile.y && y < tile.y+10){
+alert(tile.id);
+}
+}
+
+
 function create_personnage() {
    alert("Hello World");
 }
+
